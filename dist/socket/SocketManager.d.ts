958d5b1ee1c4f30be317448afb91b43bb577f147
alexdm0 <alexdemulder@gmail.com>
2020-11-16 13:12:41 +0100

    updated socket io

--- dist/socket/SocketManager.d.ts
+++ dist/socket/SocketManager.d.ts
@@ -1,5 +1,5 @@
-/// <reference types="socket.io-client" />
 /// <reference types="node" />
+/// <reference types="socket.io-client" />
 import Timeout = NodeJS.Timeout;
 import Socket = SocketIOClient.Socket;
 declare class SocketManagerClass {
